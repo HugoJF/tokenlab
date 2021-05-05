5c6d489327822c04d50bc1cecb336ddf084b937e
Hugo Jeller <hugo_jeller@hotmail.com>
2021-05-05 14:03:19 -0400

    Added Event model with policy, migration, resource, factory, controller and tests

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EventStoreRequest;
+use App\Http\Requests\EventUpdateRequest;
+use App\Http\Resources\EventResource;
+use App\Models\Event;
+use App\Models\User;
+
+class EventController extends Controller
+{
+    public function __construct()
+    {
+        $this->authorizeResource(Event::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = auth()->user();
+
+        return EventResource::collection($user->events);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(EventStoreRequest $request)
+    {
+        /** @var User $user */
+        $user = auth()->user();
+
+        $event = $user->events()->create($request->validated());
+
+        return new EventResource($event);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Event $event
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Event $event)
+    {
+        return new EventResource($event);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Event        $event
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(EventUpdateRequest $request, Event $event)
+    {
+        $event->update($request->validated());
+
+        return new EventResource($event);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Event $event
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth'])->prefix('events')->group(function () {
+    Route::get('/', [\App\Http\Controllers\EventController::class, 'index'])->name('events.index');
+    Route::get('{event}', [\App\Http\Controllers\EventController::class, 'show'])->name('events.show');
+    Route::post('/', [\App\Http\Controllers\EventController::class, 'store'])->name('events.store');
+    Route::patch('{event}', [\App\Http\Controllers\EventController::class, 'update'])->name('events.update');
+    Route::delete('{event}', [\App\Http\Controllers\EventController::class, 'destroy'])->name('events.destroy');
+});
